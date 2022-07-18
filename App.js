46e51c28fb73fe0e2c6d9091de080859e4bac73f
Ruben Gueorguiev <scoutspyz@Hotmail.com>
2022-07-18 16:49:24 +1200

    Map prototype
    - A basic MapView tested on Web and iOS.
    - Map zooms in on user location after getting permission, else it defaults coordinates of New Zealand.

--- App.js
+++ App.js
@@ -1,13 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect, createRef } from 'react'
+import MapView, { Marker, PROVIDER_GOOGLE, MAP_TYPES } from 'react-native-maps'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Platform,
+  Alert,
+  Linking,
+} from 'react-native'
+import * as Location from 'expo-location'
 
 export default function App() {
+  const [errorMsg, setErrorMsg] = useState(null)
+  const [location, setLocation] = useState({
+    latitude: -40.86268755784721,
+    longitude: 173.78307481203743,
+    latitudeDelta: 5,
+    longitudeDelta: 5,
+  })
+  const mapRef = createRef()
+
+  const openAppSettings = () => {
+    if (Platform.OS === 'ios') {
+      Linking.openURL('app-settings:')
+    } else if (Platform.OS === 'android') {
+      RNAndroidOpenSettings.appDetailsSettings()
+    }
+  }
+
+  useEffect(() => {
+    ;(async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        Alert.alert(
+          'Location permission denied.',
+          'Dasher can conviniently zoom in on your current location so that you can see the air quality in your area.',
+          [
+            {
+              text: "Don't allow",
+              onPress: () => console.log('Cancel Pressed'),
+            },
+            {
+              text: 'Allow',
+              onPress: openAppSettings,
+            },
+          ]
+        )
+        return
+      } else {
+        let currentLocation = await Location.getCurrentPositionAsync({
+          accuracy: Location.Accuracy.Balanced,
+          enableHighAccuracy: true,
+          timeInterval: 5,
+        })
+        setLocation({
+          latitude: currentLocation.coords.latitude,
+          longitude: currentLocation.coords.longitude,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1,
+        })
+      }
+    })()
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        ref={mapRef}
+        mapType={Platform.OS == 'android' ? 'none' : 'standard'}
+        provider={PROVIDER_GOOGLE}
+        region={location}
+        style={styles.map}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +84,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  errorMsg: {
+    backgroundColor: 'white',
+  },
+})
