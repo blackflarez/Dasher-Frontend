8ae291ac753bd6404e5f8a2707e23b7aa47a056b
Ruben Gueorguiev <scoutspyz@Hotmail.com>
2022-10-07 01:35:29 +1300

    Hide errors
    Hide any warnings or errors for demo purposes

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import Routes from './navigation/index'
+import { LogBox } from 'react-native'
+
+LogBox.ignoreLogs(['Warning: ...']) // Ignore log notification by message
+LogBox.ignoreAllLogs() //Ignore all log notifications
 
 export default function App() {
   return <Routes />
